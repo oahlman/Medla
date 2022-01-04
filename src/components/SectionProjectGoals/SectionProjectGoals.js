198bad674f828e3dc2e946c32d50c19feaf0ee6b
PeerDigitalAdam <80524136+PeerDigitalAdam@users.noreply.github.com>
2022-01-04 15:00:43 +0100

    Adj for mobileview

--- src/components/SectionProjectGoals/SectionProjectGoals.js
+++ src/components/SectionProjectGoals/SectionProjectGoals.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 import classNames from 'classnames';
 import css from './SectionProjectGoals.module.css';
+import { IoCheckmarkSharp } from "react-icons/io5";
 
 const SectionProjectGoals = props => {
 
@@ -16,23 +17,35 @@ const SectionProjectGoals = props => {
 
       <div className={css.steps}>
         <div className={css.step}>
-          <h2 className={css.stepTitle}>
+          <div className={css.iconPos}>
+        <IoCheckmarkSharp className={css.icon}></IoCheckmarkSharp>
+          <h2>
             <FormattedMessage id="SectionProjectGoals.part1Title" />
           </h2>
-          <p>
+          </div>
+          <div className={css.iconPos}>
+          <IoCheckmarkSharp className={css.icon}></IoCheckmarkSharp>
+          <h2>
             <FormattedMessage id="SectionProjectGoals.part1Text" />
-          </p>
+          </h2>
+          </div>
         </div>
 
 
 
         <div className={css.step}>
-          <h2 className={css.stepTitle}>
+        <div className={css.iconPos}>
+        <IoCheckmarkSharp className={css.icon}></IoCheckmarkSharp>
+          <h2>
             <FormattedMessage id="SectionProjectGoals.part3Title" />
           </h2>
-          <p>
-            <FormattedMessage id="SectionProjectGoals.part3Text"  />
-          </p>
+          </div>
+
+          <div className={css.iconPos}>
+          <IoCheckmarkSharp className={css.icon}></IoCheckmarkSharp>
+          <h2> <FormattedMessage id="SectionProjectGoals.part3Text"  />
+          </h2>
+          </div>
         </div>
       </div>
 

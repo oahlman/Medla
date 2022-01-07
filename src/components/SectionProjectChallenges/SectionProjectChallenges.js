474f5b09204f3498dfbd4e8a86c133de503f3964
PeerDigital <80488004+PeerDigital@users.noreply.github.com>
2022-01-07 10:09:49 +0100

    challenges

--- src/components/SectionProjectChallenges/SectionProjectChallenges.js
+++ src/components/SectionProjectChallenges/SectionProjectChallenges.js
@@ -15,28 +15,20 @@ const SectionProjectChallenges = props => {
       </div>
 
       <div className={css.steps}>
-        <div className={css.step}>
-          <h2 className={css.stepTitle}>
-            59% av projekt får avslag
-          </h2>
-          <p className={css.stepSubtitle}>Tillståndsprocesser är allt för långdragna och blir ofta avslagna.</p>
-          <p className={css.note}><a className={css.note} href="https://svenskvindenergi.org/pressmeddelanden/stor-investeringsvilja-i-vindkraft-men-59-procent-av-verken-avslas">Svensk Vindenergi</a></p>
-        </div>
-
-        <div className={css.step}>
+      <div className={css.step}>
           <h2 className={css.stepTitle}>
             Fossifritt 2045
           </h2>
-          <p className={css.stepSubtitle}>Svensk energiproduktion ska vara fossilfri till 2045</p>
+          <p className={css.stepSubtitle}>Svensk energiproduktion ska vara fossilfri till 2045.</p>
           <p className={css.note}><a className={css.note} href="https://fossilfrittsverige.se">Fossilfritt Sverige</a></p>
         </div>
 
         <div className={css.step}>
           <h2 className={css.stepTitle}>
-            Lokalkännedom
+            43% får veto
           </h2>
-          <p className={css.stepSubtitle}>Svårt att hitta, prioritera och kvalitetssäkra lokala leverantörer, även när de finns.</p>
-          <p className={css.note}><a className={css.note}>Interna studier</a></p>
+          <p className={css.stepSubtitle}>Andelen ansökta vindkraftprojekt som fick kommunalt veto ökade från 8% till 43% mellan 2015–2020.</p>
+          <p className={css.note}><a className={css.note} href="https://www.regeringen.se/49e6a7/contentassets/8b16a30f6de3468ab6fa640ebb23851f/en-rattssaker-vindkraftsprovning-sou-202153#page=192">Regeringen</a></p>
         </div>
 
         <div className={css.step}>
@@ -47,6 +39,11 @@ const SectionProjectChallenges = props => {
           <p className={css.note}><a className={css.note} href="http://www.svenskvindenergi.org/wp-content/blogs.dir/11/files/2011/05/Vind_2_2012.pdf">Svensk Vindenergi</a></p>
         </div>
       </div>
+
+      <div className={css.conclusion}>
+          <p className={css.conclusionSubtitle}>Samverkan med lokalt och regionalt näringsliv kan bidra till en starkare lokalförankring, effektivare projektering och en snabbare energiomställning.</p>
+          <p className={css.note}><a className={css.note}>Interna studier</a></p>
+        </div>
 
     </div>
 

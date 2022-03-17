f93e415c0ef0de94b6fe33d0b5b2d2555e499a69
PeerDigitalAdam <80524136+PeerDigitalAdam@users.noreply.github.com>
2022-03-17 16:24:33 +0100

    WIP Förväntade vinster

--- src/components/SectionProjectGoals/SectionProjectGoals.js
+++ src/components/SectionProjectGoals/SectionProjectGoals.js
@@ -3,7 +3,9 @@ import { FormattedMessage } from '../../util/reactIntl';
 import classNames from 'classnames';
 import css from './SectionProjectGoals.module.css';
 import { IoCheckmarkOutline } from "react-icons/io5";
-
+import {
+  NamedLink,
+} from '../../components';
 const SectionProjectGoals = props => {
 
   const { rootClassName, className, currentUser } = props;
@@ -45,12 +47,18 @@ const SectionProjectGoals = props => {
             <FormattedMessage id="SectionProjectGoals.part3Title" />
           </div>
           </div>
-
-         
         </div>
       </div>
+      <div className={css.buttonAlign}>
+      
+      <div className={css.titleCTA}>
+        <FormattedMessage id="SectionProjectGoals.titleLineTwoCTA" />
 
-
+      </div>
+      <NamedLink name='SignupPage' className={css.heroButton}>
+                <FormattedMessage id="ConnectCompanyPage.connectProjectOwner" />
+              </NamedLink>
+      </div>    
     </div>
   );
 };

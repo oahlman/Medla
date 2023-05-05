0ca2a4b202b9d0c20720f92c73dc8cf0d3e94f9e
oahlman <oskar.ahlman@peerdigital.se>
2023-05-05 22:33:17 +0200

    Chat wip

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,16 +9,12 @@ const Chat = () => {
   const [isChatVisible, setIsChatVisible] = useState(false);
   const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
 
-  console.log('key', apiKey)
+  const medlaLinkDecorator = (href, text, key) => (
+    <a href={href} key={key} target="_blank" rel="noopener noreferrer">
+      {text}
+    </a>
+  );
 
-  const medlaLinkDecorator = (href, text, key) => {
-    return (
-      <a href={href} key={key} target="_blank" rel="noopener noreferrer">
-        {text}
-      </a>
-    );
-  };
-  
   const messageContainerRef = useRef(null);
 
   const [isWaitingForResponse, setIsWaitingForResponse] = useState(false);
@@ -32,12 +28,12 @@ const Chat = () => {
   const sendMessage = async (e) => {
     e.preventDefault();
     if (!message) return;
-  
+
     setMessages((prevMessages) => [...prevMessages, { user: 'Du', text: message }]);
-  
+
     setMessage('');
     setIsWaitingForResponse(true);
-  
+
     try {
       const response = await callChatGPT(message);
       setMessages((prevMessages) => [...prevMessages, { user: 'Bot', text: response }]);
@@ -47,8 +43,6 @@ const Chat = () => {
       setIsWaitingForResponse(false);
     }
   };
-  
-  
 
   const callChatGPT = async (message) => {
     try {
@@ -168,7 +162,9 @@ const Chat = () => {
             onClick={() => setIsChatVisible(false)}
             className={css.closeChatButton}
           >
-            ×
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 240 240" fill="none">
+              <path d="M30 30 L210 210 M30 210 L210 30" stroke="white" stroke-width="25"/>
+            </svg>
           </button>
           <div className={css.messageContainer} ref={messageContainerRef}>
             {messages.map((msg, index) => (
@@ -198,17 +194,21 @@ const Chat = () => {
                 placeholder="Prova att fråga 'Hur anmäler jag mig?' eller 'Vilka tjänster erbjuder ni?'"
                 className={css.input}
               />
-              <button type="submit" className={css.button}>Skicka</button>
+              <button className={css.sendButton}>
+                <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M4.563 14.605l9.356-5.402c1-.577 1-2.02 0-2.598L4.563 1.203a1.5 1.5 0 00-2.25 1.3v10.803a1.5 1.5 0 002.25 1.3zM6.51 8.387L2.313 9.512V6.297L6.51 7.42c.494.133.494.834 0 .966z"></path>
+                </svg>
+              </button>
             </div>
           </form>
-        </div>
+          </div>
       )}
       {!isChatVisible && (
-        <button
-          onClick={() => setIsChatVisible(true)}
-          className={css.openChatButton}
-        >
-          Öppna Chatt
+        <button onClick={() => setIsChatVisible(true)} className={css.openChatButton}>
+        <svg id="woot-widget-bubble-icon" width="24" height="24" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M240.808 240.808H122.123C56.6994 240.808 3.45695 187.562 3.45695 122.122C3.45695 56.7031 56.6994 3.45697 122.124 3.45697C187.566 3.45697 240.808 56.7031 240.808 122.122V240.808Z" fill="#FFFFFF">
+          </path>
+        </svg>
         </button>
       )}
     </>

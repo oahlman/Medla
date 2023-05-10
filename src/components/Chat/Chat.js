e0b31f9aa500dc937824b3d99d6dba35fba1f843
oahlman <oskar.ahlman@peerdigital.se>
2023-05-10 22:10:44 +0200

    Icons

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Linkify from 'react-linkify';
 import css from './Chat.module.css';
 import chatContent from './ChatContent'
+import { bubbleIcon, sendIcon } from './icons'
 
 const Chat = () => {
   const [message, setMessage] = useState('');
@@ -132,9 +133,7 @@ const Chat = () => {
                 className={css.input}
               />
               <button className={css.sendButton}>
-                <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M4.563 14.605l9.356-5.402c1-.577 1-2.02 0-2.598L4.563 1.203a1.5 1.5 0 00-2.25 1.3v10.803a1.5 1.5 0 002.25 1.3zM6.51 8.387L2.313 9.512V6.297L6.51 7.42c.494.133.494.834 0 .966z"></path>
-                </svg>
+                {sendIcon}
               </button>
             </div>
           </form>
@@ -142,10 +141,7 @@ const Chat = () => {
       )}
       {!isChatVisible && (
         <button onClick={() => setIsChatVisible(true)} className={css.openChatButton}>
-        <svg id="woot-widget-bubble-icon" width="24" height="24" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M240.808 240.808H122.123C56.6994 240.808 3.45695 187.562 3.45695 122.122C3.45695 56.7031 56.6994 3.45697 122.124 3.45697C187.566 3.45697 240.808 56.7031 240.808 122.122V240.808Z" fill="#FFFFFF">
-          </path>
-        </svg>
+        {bubbleIcon}
         </button>
       )}
     </>
